c9c52b94490c0dfb7362b249dbc08db8abd8d92e
PaylineGitHub <github@payline.com>
2017-12-02 21:29:44 +0100

    don't send empty private data or order detail

--- examples/arraySet/order.php
+++ examples/arraySet/order.php
@@ -13,28 +13,50 @@ $array['order']['deliveryExpectedDate'] = $_POST['deliveryExpectedDate'];
 $array['order']['deliveryExpectedDelay'] = $_POST['deliveryExpectedDelay'];
 
 // ORDER DETAILS
+if($_POST['orderDetailRef1'] != '' && 
+$_POST['orderDetailPrice1'] != '' && 
+$_POST['orderDetailQuantity1'] != '' && 
+$_POST['orderDetailComment1'] != '' && 
+$_POST['orderDetailCategory1'] != '' && 
+$_POST['orderDetailBrand1'] != '' && 
+$_POST['orderDetailSubcategory1_1'] != '' && 
+$_POST['orderDetailSubcategory2_1'] != '' && 
+$_POST['orderDetailAdditionalData1'] != '' && 
+$_POST['orderDetailTaxRate1']){
 $item1 = array();
-$item1['ref'] = $_POST['orderDetailRef1'];
-$item1['price'] = $_POST['orderDetailPrice1'];
-$item1['quantity'] = $_POST['orderDetailQuantity1'];
-$item1['comment'] = $_POST['orderDetailComment1'];
-$item1['category'] = $_POST['orderDetailCategory1'];
-$item1['brand'] = $_POST['orderDetailBrand1'];
-$item1['subcategory1'] = $_POST['orderDetailSubcategory1_1'];
-$item1['subcategory2'] = $_POST['orderDetailSubcategory2_1'];
-$item1['additionalData'] = $_POST['orderDetailAdditionalData1'];
-$item1['taxRate'] = $_POST['orderDetailTaxRate1'];
-$payline->addOrderDetail($item1);
+    $item1['ref'] = $_POST['orderDetailRef1'];
+    $item1['price'] = $_POST['orderDetailPrice1'];
+    $item1['quantity'] = $_POST['orderDetailQuantity1'];
+    $item1['comment'] = $_POST['orderDetailComment1'];
+    $item1['category'] = $_POST['orderDetailCategory1'];
+    $item1['brand'] = $_POST['orderDetailBrand1'];
+    $item1['subcategory1'] = $_POST['orderDetailSubcategory1_1'];
+    $item1['subcategory2'] = $_POST['orderDetailSubcategory2_1'];
+    $item1['additionalData'] = $_POST['orderDetailAdditionalData1'];
+    $item1['taxRate'] = $_POST['orderDetailTaxRate1'];
+    $payline->addOrderDetail($item1);
+}
 
-$item2 = array();
-$item2['ref'] = $_POST['orderDetailRef2'];
-$item2['price'] = $_POST['orderDetailPrice2'];
-$item2['quantity'] = $_POST['orderDetailQuantity2'];
-$item2['comment'] = $_POST['orderDetailComment2'];
-$item2['category'] = $_POST['orderDetailCategory2'];
-$item2['brand'] = $_POST['orderDetailBrand2'];
-$item2['subcategory1'] = $_POST['orderDetailSubcategory1_2'];
-$item2['subcategory2'] = $_POST['orderDetailSubcategory2_2'];
-$item2['additionalData'] = $_POST['orderDetailAdditionalData2'];
-$item2['taxRate'] = $_POST['orderDetailTaxRate2'];
-$payline->addOrderDetail($item2);
+if($_POST['orderDetailRef2'] != '' && 
+$_POST['orderDetailPrice2'] != '' && 
+$_POST['orderDetailQuantity2'] != '' && 
+$_POST['orderDetailComment2'] != '' && 
+$_POST['orderDetailCategory2'] != '' && 
+$_POST['orderDetailBrand2'] != '' && 
+$_POST['orderDetailSubcategory1_2'] != '' && 
+$_POST['orderDetailSubcategory2_2'] != '' && 
+$_POST['orderDetailAdditionalData2'] != '' && 
+$_POST['orderDetailTaxRate2']){
+    $item2 = array();
+    $item2['ref'] = $_POST['orderDetailRef2'];
+    $item2['price'] = $_POST['orderDetailPrice2'];
+    $item2['quantity'] = $_POST['orderDetailQuantity2'];
+    $item2['comment'] = $_POST['orderDetailComment2'];
+    $item2['category'] = $_POST['orderDetailCategory2'];
+    $item2['brand'] = $_POST['orderDetailBrand2'];
+    $item2['subcategory1'] = $_POST['orderDetailSubcategory1_2'];
+    $item2['subcategory2'] = $_POST['orderDetailSubcategory2_2'];
+    $item2['additionalData'] = $_POST['orderDetailAdditionalData2'];
+    $item2['taxRate'] = $_POST['orderDetailTaxRate2'];
+    $payline->addOrderDetail($item2);
+}
--- examples/arraySet/privateDataList.php
+++ examples/arraySet/privateDataList.php
@@ -3,5 +3,7 @@ for($i=1;$i<9;$i++){
     $privateData = array();
     $privateData['key'] = $_POST['privateDataKey0'.$i];
     $privateData['value'] = $_POST['privateDataValue0'.$i];
-    $payline->addPrivateData($privateData);
+    if($privateData['key'] != ''){
+        $payline->addPrivateData($privateData);
+    }
 }
